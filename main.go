a243075f1c9211e0226efe37cc4013f1212f8d00
Zenk41 <ardhidhani@gmail.com>
2022-09-27 15:17:41 +0700

    Initial Project Praktikum ORM Part 1

---
+++ main.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func init() {
+	InitDB()
+	InitialMigration()
+}
+
+type Config struct {
+	DB_Username string
+	DB_Password string
+	DB_Port     string
+	DB_Host     string
+	DB_Name     string
+}
+
+func InitDB() {
+
+	config := Config{
+		DB_Username: "root",
+		DB_Password: "@root123",
+		DB_Port:     "3306",
+		DB_Host:     "localhost",
+		DB_Name:     "crud_go",
+	}
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		config.DB_Username,
+		config.DB_Password,
+		config.DB_Host,
+		config.DB_Port,
+		config.DB_Name,
+	)
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+}
+
+type User struct {
+	gorm.Model
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+}
+
+func InitialMigration() {
+	DB.AutoMigrate(&User{})
+}
+
+// get all users
+func GetUsersController(c echo.Context) error {
+	var users []User
+
+	if err := DB.Find(&users).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get all users",
+		"users":   users,
+	})
+}
+
+// get user by id
+func GetUserController(c echo.Context) error {
+	// your solution here
+	var user []User
+	id, _ := strconv.Atoi(c.Param("id"))
+	if err := DB.First(&user, "id = ?", id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success create new user",
+		"user":    user,
+	})
+}
+
+// create new user
+func CreateUserController(c echo.Context) error {
+	user := User{}
+	c.Bind(&user)
+
+	if err := DB.Save(&user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success create new user",
+		"user":    user,
+	})
+}
+
+// delete user by id
+func DeleteUserController(c echo.Context) error {
+	// your solution here
+	var user []User
+	id, _ := strconv.Atoi(c.Param("id"))
+	if err := DB.Where("id = ?", id).First(&user).Delete(&user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	// // return softdelete
+	// if err := DB.Unscoped().First(&user, id).Error; err != nil {
+	// 	return echo.NewHTTPError(http.StatusBadRequest, map[string]interface{}{
+	// 		"message": "succes get deleted user",
+	//   "deleted_user" : user
+	// 	})
+	// }
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success delete user",
+		"user":    user,
+	})
+}
+
+// update user by id
+func UpdateUserController(c echo.Context) error {
+	// your solution here
+	user := User{}
+	if err := c.Bind(&user); err != nil {
+		return err
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	if err := DB.Model(&user).Where("id=?", id).Updates(&user).First(&user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success create new user",
+		"user":    user,
+	})
+}
+
+func main() {
+	// create a new echo instance
+	e := echo.New()
+	// Route / to handler function
+	e.GET("/users", GetUsersController)
+	e.GET("/users/:id", GetUserController)
+	e.POST("/users", CreateUserController)
+	e.DELETE("/users/:id", DeleteUserController)
+	e.PUT("/users/:id", UpdateUserController)
+
+	// start the server, and log if it fails
+	e.Logger.Fatal(e.Start(":8000"))
+}
